e7ef77afeb5d9df9e301b27c154875e2d7e69d96
tayyabnaqvi05 <138791403+tayyabnaqvi05@users.noreply.github.com>
2023-08-10 12:51:18 +0500

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -23,7 +23,7 @@ const Navi = () => {
                   </div>
                 </li>
                 <li className="nav-item active">
-                  <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                  <a className="nav-link text-danger" href="/">SIGN IN<span className="sr-only">(current)</span></a>
                 </li>
                 
               </ul>
